0a0e2408f290c0d744a36b4df573df501806e61e
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-06-08 01:36:30 +0800

    Almost Finished All Modules

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,6 +1,7 @@
 ﻿using AlphaZero.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -53,36 +54,61 @@ namespace AlphaZero.Controllers
             return View(reminds);
         }
 
-        // POST: User/Edit/5
         [HttpPost]
-        public ActionResult Edit([Bind(Include = "reminds_id,reminder_title,reminder_desc,reminder_date")] reminds reminds)
+        public ActionResult Edit(reminds reminds)
         {
             if (ModelState.IsValid)
             {
-                // Retrieve the existing investor record based on the user_id
-                reminds reminds1 = db.reminds.SingleOrDefault(i => i.reminder_id == reminds.reminder_id);
+                // Retrieve the existing record based on the reminder_id
+                reminds existingReminder = db.reminds.SingleOrDefault(i => i.reminder_id == reminds.reminder_id);
 
-                if (reminds1 != null)
+                if (existingReminder != null)
                 {
-                    reminds1.reminder_title = reminds.reminder_title;
-                    reminds1.reminder_desc = reminds.reminder_desc;
-                    reminds1.reminder_date = reminds.reminder_date;
+                    existingReminder.reminder_title = reminds.reminder_title;
+                    existingReminder.reminder_desc = reminds.reminder_desc;
+                    existingReminder.reminder_date = reminds.reminder_date;
 
-                    db.Entry(reminds1).State = System.Data.Entity.EntityState.Modified;
+                    // Update the attributes only if landlord_id and tenant_id are null
+                    if (reminds.landlord_id == null)
+                    {
+                        existingReminder.landlord_id = null;
+                    }
+                    else
+                    {
+                        existingReminder.landlord_id = reminds.landlord_id;
+                    }
+
+                    if (reminds.tenant_id == null)
+                    {
+                        existingReminder.tenant_id = null;
+                    }
+                    else
+                    {
+                        existingReminder.tenant_id = reminds.tenant_id;
+                    }
+
+                    db.Entry(existingReminder).State = EntityState.Modified;
                     db.SaveChanges();
                     return RedirectToAction("Index");
                 }
 
                 return RedirectToAction("Index");
             }
+
             return View(reminds);
         }
+
+
 
         [HttpPost]
         public ActionResult Create(reminds reminds)
         {
             if (ModelState.IsValid)
             {
+                reminds.reminder_status = 0;
+                reminds.tenant_id = null;
+                reminds.landlord_id = null;
+
                 db.reminds.Add(reminds);
                 db.SaveChanges();
                 return RedirectToAction("Index");
