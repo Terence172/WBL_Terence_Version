ec269b3848135e6648e0fe3bc7e688459c16d073
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-06-13 16:53:52 +0800

    Finishing Touches

--- Models/Model1.Context.cs
+++ Models/Model1.Context.cs
@@ -37,17 +37,6 @@ namespace AlphaZero.Models
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<tenant> tenants { get; set; }
         public virtual DbSet<user> users { get; set; }
-        public virtual DbSet<AggregatedCounter> AggregatedCounters { get; set; }
-        public virtual DbSet<Counter> Counters { get; set; }
-        public virtual DbSet<Hash> Hashes { get; set; }
-        public virtual DbSet<Job> Jobs { get; set; }
-        public virtual DbSet<JobParameter> JobParameters { get; set; }
-        public virtual DbSet<JobQueue> JobQueues { get; set; }
-        public virtual DbSet<List> Lists { get; set; }
-        public virtual DbSet<Schema> Schemata { get; set; }
-        public virtual DbSet<Server> Servers { get; set; }
-        public virtual DbSet<Set> Sets { get; set; }
-        public virtual DbSet<State> States { get; set; }
         public virtual DbSet<sysConfig> sysConfigs { get; set; }
     }
 }
